450f2f818ba88c32597a90223dc1c13c0a2e5065
Zeynep Türkan <zeynepturkann@gmail.com>
2025-07-11 13:14:45 +0300

    feat: ana karakter animasyonları ve kodları yapıldı.

---
+++ Assets/Ethan the Hero/Script/PlayerAttackMethod.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dylan
+{
+	public class PlayerAttackMethod : MonoBehaviour
+	{
+		#region FIELD
+
+		private PlayerAnimation playerAnim;
+		private PlayerMovement playerMv;
+		private Animator myAnim;
+		private Rigidbody2D myBody;
+
+		[Header("Basic Attack")]
+		public float basicAttack01Power = 0.5f;
+		public float basicAttack02Power = 0.5f;
+		public float basicAttack03Power = 0.9f;
+
+		private bool atkButtonClickedOnAtk01;
+		private bool atkButtonClickedOnAtk02;
+		private bool atkButtonClickedOnAtk03;
+
+		private const string attack01 = "Attack01";
+		private const string attack02 = "Attack02";
+		private const string attack03 = "Attack03";
+		private const string notAttacking = "NotAttacking";
+
+		#endregion
+		void Awake()
+		{
+			myAnim = GetComponent<Animator>();
+			playerAnim = GetComponent<PlayerAnimation>();
+			myBody = GetComponent<Rigidbody2D>();
+			playerMv = GetComponent<PlayerMovement>();
+		}
+		void Update()
+		{
+			if (playerMv.isDashing) 
+				return;
+			BasicAttackCombo();
+		}
+        void FixedUpdate()
+        {
+            if (playerMv.isDashing)
+            BasicAttackMethod();
+        }
+        #region BASIC ATTACK
+
+        private void BasicAttackCombo()
+		{
+			//Combo attack mekanik
+			if (Input.GetMouseButtonDown(0) && !myAnim.GetCurrentAnimatorStateInfo(0).IsName("Attack01") && !myAnim.GetCurrentAnimatorStateInfo(0).IsName("Attack02") && !myAnim.GetCurrentAnimatorStateInfo(0).IsName("Attack03") && playerMv.grounded)
+				myAnim.SetTrigger(attack01);
+			//Set combo attack 01 
+			if (myAnim.GetCurrentAnimatorStateInfo(0).IsName("Attack01"))
+			{
+				if (Input.GetMouseButtonDown(0))
+					atkButtonClickedOnAtk01 = true;
+
+				if (myAnim.GetCurrentAnimatorStateInfo(0).normalizedTime >= .8 && atkButtonClickedOnAtk01)
+				{
+					myAnim.SetTrigger(attack02);
+					atkButtonClickedOnAtk01 = false;
+
+				}
+				else if (myAnim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1 && !atkButtonClickedOnAtk01)
+					myAnim.SetTrigger(notAttacking);
+			}
+			//Set combo attack 02
+			if (myAnim.GetCurrentAnimatorStateInfo(0).IsName("Attack02"))
+			{
+				if (Input.GetMouseButtonDown(0))
+					atkButtonClickedOnAtk02 = true;
+
+				if (myAnim.GetCurrentAnimatorStateInfo(0).normalizedTime >= .8 && atkButtonClickedOnAtk02)
+				{
+					myAnim.SetTrigger(attack03);
+					atkButtonClickedOnAtk02 = false;
+
+				}
+				else if (myAnim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1 && !atkButtonClickedOnAtk02)
+					myAnim.SetTrigger(notAttacking);
+			}
+			//Set combo attack 03
+			if (myAnim.GetCurrentAnimatorStateInfo(0).IsName("Attack03"))
+			{
+				if (Input.GetMouseButtonDown(0))
+					atkButtonClickedOnAtk03 = true;
+
+				if (myAnim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1 && atkButtonClickedOnAtk03)
+				{
+					myAnim.SetTrigger(attack01);
+					atkButtonClickedOnAtk03 = false;
+
+				}
+				else if (myAnim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1 && !atkButtonClickedOnAtk03)
+					myAnim.SetTrigger(notAttacking);
+			}
+		}
+		private void BasicAttackMethod()
+		{
+			// MOVE + ATTACK 
+			if (transform.localScale.x == 1)
+			{
+				if (myAnim.GetCurrentAnimatorStateInfo(0).IsName("Attack01"))
+					myBody.linearVelocity = new Vector2(basicAttack01Power, myBody.linearVelocity.y);
+
+				if (myAnim.GetCurrentAnimatorStateInfo(0).IsName("Attack02"))
+					myBody.linearVelocity = new Vector2(basicAttack02Power, myBody.linearVelocity.y);
+
+				if (myAnim.GetCurrentAnimatorStateInfo(0).IsName("Attack03"))
+					myBody.linearVelocity = new Vector2(basicAttack03Power, myBody.linearVelocity.y);
+			}
+			else
+			{
+				if (myAnim.GetCurrentAnimatorStateInfo(0).IsName("Attack01"))
+					myBody.linearVelocity = new Vector2(-basicAttack01Power, myBody.linearVelocity.y);
+
+				if (myAnim.GetCurrentAnimatorStateInfo(0).IsName("Attack02"))
+					myBody.linearVelocity = new Vector2(-basicAttack02Power, myBody.linearVelocity.y);
+
+				if (myAnim.GetCurrentAnimatorStateInfo(0).IsName("Attack03"))
+					myBody.linearVelocity = new Vector2(-basicAttack03Power, myBody.linearVelocity.y);
+			}
+
+		}
+		#endregion
+
+
+	}
+}
---
+++ Assets/Ethan the Hero/Script/PlayerMovement/PlayerMovementData.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+// Bu satır, Unity'de Create menüsünde görünmesini sağlar.
+[CreateAssetMenu(fileName = "PlayerMovementData", menuName = "ScriptableObjects/Player Movement Data", order = 1)]
+public class PlayerMovementData : ScriptableObject
+{
+    [Header("Run")]
+    public float runMaxSpeed = 8f;
+    public float runAccelAmount = 0.8f;
+    public float runDeccelAmount = 0.5f;
+    [Header("Jump")]
+    public float jumpHeight = 8f;
+    [Header("Dash")]
+    public float dashPower = 8f;
+    public float dashingTime = 0.2f;
+    public float dashingCoolDown = 1f;
+    [Header("Air Control")]
+    public float accelInAir = 0.5f;
+    public float deccelInAir = 0.5f;
+    public bool doConserveMomentum = true;
+}
